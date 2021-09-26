4654833cd5d35245d4f3b8896187c3d17398ff97
LeatherShark <84489415+LeatherShark@users.noreply.github.com>
2021-09-26 18:08:26 +0530

    Change if-else block

--- test.py
+++ test.py
@@ -5,9 +5,11 @@ print("This print funcition was added in the new branch")
 a = 100
 
 if a < 1000:
-    print("Hello")
+    print("Less than 1000")
+elif a > 1000:
+    print("Greater than 1000")
 else:
-    print("World!")
+    print("Equal to 1000")
 
 for _ in range(1, 101):
     print("Hello, World!")
