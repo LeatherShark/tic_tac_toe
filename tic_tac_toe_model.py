dd8c42a352c33268cee38afcd3712377618a7e92
LeatherShark <84489415+LeatherShark@users.noreply.github.com>
2021-09-27 08:22:59 +0530

    Add functions to get state at rows columns and diagonals

--- tic_tac_toe_model.py
+++ tic_tac_toe_model.py
@@ -1,45 +1,104 @@
 import numpy as np
+from random import choice
 
 
 class SetStateError(Exception):
     """Raised for State that has already been assigned."""
 
 
-class TicTacToeGrid:
-    """Representation of a Tic-Tac-Toe Grid"""
+class Grid:
+    """Representation of a Grid for Board Game."""
 
     def __init__(self):
-        """Initialize the Grid"""
+        """Initialize the Grid."""
 
         self.grid = np.array(
-            [{"x": x, "y": y, "state": 0} for x in range(3) for y in range(3)]
+            [{"row": row, "column": column, "state": 0} for row in range(3) for column in range(3)]
         ).reshape(3, 3)
 
-    def getStateAt(self, xCoord: int, yCoord: int) -> int:
-        """Return the State of the Cell"""
+    def getStateAt(self, row: int, column: int) -> int:
+        """Returns State of Cell."""
 
-        return self.grid[xCoord][yCoord]["state"]
+        return self.grid[row][column]["state"]
 
-    def setStateAt(self, xCoord: int, yCoord: int, state: int):
-        """Sets the State at Cell"""
+    def setStateAt(self, row: int, column: int, state: int) -> None:
+        """Sets State at Cell."""
 
         # Checks if State is Unassigned
-        if self.getStateAt(xCoord, yCoord) != 0:
+        if self.getStateAt(row, column) != 0:
             raise SetStateError("Cannot Assign State to a Cell twice.")
 
-        self.grid[xCoord][yCoord]["state"] = state
+        self.grid[row][column]["state"] = state
 
     def getStateGrid(self):
-        """Returns Grid of States"""
+        """Returns Grid of States."""
 
         return np.array(
-            [self.getStateAt(xCoord=x, yCoord=y) for x in range(3) for y in range(3)]
+            [self.getStateAt(row=row, column=column) for row in range(3) for column in range(3)]
         ).reshape(3, 3)
 
+    def getStateColumn(self, column: int):
+        """Returns a numpy array of States of the given column."""
 
-testGrid = TicTacToeGrid()
+        # return self.grid
 
-print(testGrid.grid)
-testGrid.setStateAt(0, 0, 1)
-testGrid.setStateAt(0, 0, 1)
-print(testGrid.getStateGrid())
+        # return np.array(
+        #     [self.getStateAt(row=row, column=column) for row in range(3)]
+        # ).reshape(3, 1)
+
+    def getStateRow(self, row: int):
+        """Returns a numpy array of States of the given row."""
+
+        return np.array(
+            [self.getStateAt(row=row, column=column) for column in range(3)]
+        ).reshape(1, 3)
+    
+    def getStateLeftRightDiagonal(self):
+        """Returns a numpy array of States of the Left-Right Diagonal"""
+        print("Hello")
+        return self.grid.diagonal(-1, 0, 1)
+
+        # return np.array([
+        #     self.getStateAt(row=0, column=0),
+        #     self.getStateAt(row=1, column=1),
+        #     self.getStateAt(row=2, column=2)
+        # ]).reshape(3, 1)
+        
+
+    def getStateRightLeftDiagonal(self):
+        """Returns a numpy array of States of the Right-Left Diagonal"""
+
+        return np.array([
+            self.getStateAt(row=0, column=2),
+            self.getStateAt(row=1, column=1),
+            self.getStateAt(row=2, column=0)
+        ]).reshape(3, 1)
+
+
+def tests():
+    testGrid = Grid()
+
+    print(testGrid.grid)
+    for xCoord in range(3):
+        for yCoord in range(3):
+            randomState = choice([-1, 1, 0])
+            print(f"{randomState:.0f} at row {xCoord}, column {yCoord}")
+            testGrid.setStateAt(xCoord, yCoord, randomState)
+            # print(testGrid.getStateGrid())
+    # testGrid.setStateAt(0, 0, 1)
+    print(testGrid.getStateGrid())
+
+    print(testGrid.getStateColumn(0))
+    print(testGrid.getStateColumn(1))
+    print(testGrid.getStateColumn(2))
+
+    print(testGrid.getStateRow(0))
+    print(testGrid.getStateRow(1))
+    print(testGrid.getStateRow(2))
+
+    print(testGrid.getStateLeftRightDiagonal())
+    print(testGrid.getStateRightLeftDiagonal())
+
+
+if __name__ == "__main__":
+    tests()
